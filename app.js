4920df250acb2c575c8bfbc7a5da21b06c630333
Henry <henryjamesbeck@gmail.com>
2023-05-02 13:06:54 +0400

    First commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const sections = document.querySelectorAll(".section");
+const sectBtns = document.querySelectorAll(".controls");
+const sectBtn = document.querySelectorAll(".control");
+const allSections = document.querySelector(".main-content");
+
+function PageTransitions() {
+
+    //Button Click Active Class
+    for(let i = 0; i < sectBtn.length; i++) {
+        sectBtn[i].addEventListener("click", function() {
+            let currentBtn = document.querySelector(".active-btn");
+            currentBtn.className = currentBtn.className.replace("active-btn", "");
+            this.className += " active-btn";
+        });
+    }
+
+    //Sections Active Class
+    allSections.addEventListener("click", (event) => {
+        const id = event.target.dataset.id;
+        if(id) {
+            //Remove selected from the other button
+            sectBtns.forEach((btn) => {
+                btn.classList.remove("active");
+            });
+
+            event.target.classList.add("active");
+
+            //Hide other sections
+            sections.forEach((sections) => {
+                sections.classList.remove("active");
+            });
+
+            const element = document.getElementById(id);
+            element.classList.add("active");
+
+        }
+    });
+
+}
+
+PageTransitions();
+
+
+
+
+
+
+
+
+
+
+
