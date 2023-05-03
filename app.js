c0da95897328b42f828cf176b77fe99d9f1926ca
Henry <henryjamesbeck@gmail.com>
2023-05-03 18:27:08 +0400

    Implemented additional changes suggested by Ria

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const sections = document.querySelectorAll(".section");
 const sectBtns = document.querySelectorAll(".controls");
 const sectBtn = document.querySelectorAll(".control");
